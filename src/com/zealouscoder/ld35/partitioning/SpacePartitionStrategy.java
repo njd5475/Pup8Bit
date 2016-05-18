484cc4850ab82fa85950ec3fde81df812a593012
zCoder <njd5475@gmail.com>
2016-05-17 22:32:29 -0400

    Moving paritioning strategy.

--- src/com/zealouscoder/ld35/partitioning/SpacePartitionStrategy.java
+++ src/com/zealouscoder/ld35/partitioning/SpacePartitionStrategy.java
@@ -1,25 +1,39 @@
-package com.zealouscoder.ld35;
+package com.zealouscoder.ld35.partitioning;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import com.zealouscoder.ld35.movement.GamePosition;
 import com.zealouscoder.ld35.rendering.GameObjectBound;
 import com.zealouscoder.ld35.rendering.Renderable;
 
 public class SpacePartitionStrategy {
 
+	private double binSize = 0;
+	
 	private Map<Integer, Set<Renderable>> bins = new HashMap<Integer, Set<Renderable>>();
 	
-	public void add(Renderable r) {
+	public SpacePartitionStrategy(double binSize) {
+		this.binSize = binSize;
 		
 	}
 	
-	public Renderable[] get(GameObjectBound bounds) {
+	public void add(Renderable r) {
+	}
+	
+	public Renderable[] get(GamePosition pos, GameObjectBound bound) {
+		
 		return null;
 	}
 	
 	private int[] getBins(Renderable r) {
+		int topleft = 0;
+		int topright = 0;
+		int bottomleft = 0;
+		int bottomright = 0;
+		
 		return new int[]{};
 	}
+	
 }
